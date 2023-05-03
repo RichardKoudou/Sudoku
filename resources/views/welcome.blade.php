924282d33c4fbfba79aaa935016fbaf77a236039
benaja-bendo <benaja.bendo02@gmail.com>
2023-05-03 09:05:47 +0200

    creation du sudoku sur la home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,105 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-</x-app-layout>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        <main>
+            <table id="sudoku">
+                <tbody>
+                    @foreach ($sudokuGrid as $row)
+                    <tr>
+                        @foreach ($row as $cell)
+                        <td>
+                            <input type="text" maxlength="1" class="sudoku-input" value="{{ $cell }}" {{ $cell !==0
+                                ? 'readonly' : '' }}>
+                        </td>
+                        @endforeach
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </main>
+
+        <style>
+            /* with css reset */
+            * {
+                box-sizing: border-box;
+            }
+
+            table {
+                margin: 10px;
+            }
+
+            tr:first-child td {
+                border-top-color: black;
+            }
+
+            tr:nth-child(3n) td {
+                border-bottom-color: black;
+            }
+
+            td {
+                border: 1px solid lightgrey;
+                height: 40px;
+                width: 40px;
+            }
+
+            td:first-child {
+                border-left-color: black;
+            }
+
+            td:nth-child(3n) {
+                border-right-color: black;
+            }
+
+            input {
+                padding: 0;
+                text-align: center;
+                border: 0;
+                height: 40px;
+                width: 40px;
+                text-align: center;
+            }
+
+            input:hover {
+                background: #ffffff;
+            }
+        </style>
+
+        <script>
+            const sudoku = document.getElementById('sudoku');
+            const inputs = sudoku.getElementsByTagName('input');
+
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].addEventListener('keydown', function(e) {
+                    if (e.keyCode === 38) {
+                        inputs[i - 9].focus();
+                    } else if (e.keyCode === 40) {
+                        inputs[i + 9].focus();
+                    } else if (e.keyCode === 37) {
+                        inputs[i - 1].focus();
+                    } else if (e.keyCode === 39) {
+                        inputs[i + 1].focus();
+                    }
+                });
+            }
+        </script>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,73 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+function generateSudokuGrid()
+{
+    // Créer un tableau vide pour représenter la grille
+    $grid = array_fill(0, 9, array_fill(0, 9, 0));
+
+    // Remplir la première ligne de la grille avec neuf nombres aléatoires
+    $numbers = range(1, 9);
+    shuffle($numbers);
+    for ($i = 0; $i < 9; $i++) {
+        $grid[0][$i] = $numbers[$i];
+    }
+
+    // Remplir le reste de la grille
+    for ($row = 1; $row < 9; $row++) {
+        for ($col = 0; $col < 9; $col++) {
+            // Décaler les nombres vers la droite
+            for ($i = 8; $i > 0; $i--) {
+                $grid[$row][$i] = $grid[$row][$i - 1];
+            }
+            $grid[$row][0] = $grid[$row - 1][6];
+            $grid[$row][1] = $grid[$row - 1][7];
+            $grid[$row][2] = $grid[$row - 1][8];
+
+            // Échanger les colonnes 1-3, 4-6 et 7-9 aléatoirement
+            for ($i = 0; $i < 3; $i++) {
+                $col1 = $i * 3 + rand(0, 2);
+                $col2 = $i * 3 + rand(0, 2);
+                while ($col2 == $col1) {
+                    $col2 = $i * 3 + rand(0, 2);
+                }
+                $temp = $grid[$row][$col1];
+                $grid[$row][$col1] = $grid[$row][$col2];
+                $grid[$row][$col2] = $temp;
+            }
+        }
+    }
+
+    // Échanger les rangées et les colonnes pour mélanger la grille
+    for ($i = 0; $i < 20; $i++) {
+        $row1 = rand(0, 2) * 3 + rand(0, 2);
+        $row2 = rand(0, 2) * 3 + rand(0, 2);
+        while ($row2 == $row1) {
+            $row2 = rand(0, 2) * 3 + rand(0, 2);
+        }
+        $col1 = rand(0, 2) * 3 + rand(0, 2);
+        $col2 = rand(0, 2) * 3 + rand(0, 2);
+        while ($col2 == $col1) {
+            $col2 = rand(0, 2) * 3 + rand(0, 2);
+        }
+        $temp = $grid[$row1][$col1];
+        $grid[$row1][$col1] = $grid[$row2][$col2];
+        $grid[$row2][$col2] = $temp;
+    }
+
+    return $grid;
+}
 
 Route::get('/', function () {
-    return view('welcome');
+    // $grids = generateSudokuGrid();
+    $puzzle = new Xeeeveee\Sudoku\Puzzle();
+    $puzzle->generatePuzzle();
+    $puzzle = $puzzle->getPuzzle();
+    // dd($puzzle);
+    return view('welcome', [
+        'sudokuGrid' => $puzzle,
+    ]);
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +82,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
